026d825e6e2abd121fc0a9bcc598bbf09a838c1d
yasmindeoliveira <yaz.oliveira01@gmail.com>
2022-11-11 15:33:43 +0100

    completato esercizio

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+let myContainer = document.querySelector(".my-container");
+
 for (let i=1; i <=100; i++) {
     
     if (i % 5 == 0 && i % 3 == 0){
         console.log("FizzBuzz");
+        myContainer.innerHTML += '<div class="square"><p>FizzBuzz</p></div>'
     } else if (i % 5 == 0){
         console.log("Buzz");
+        myContainer.innerHTML += '<div class="square"><p>Buzz</p></div>'
     } else if (i % 3 == 0){
         console.log("Fizz");
+        myContainer.innerHTML += '<div class="square"><p>Fizz</p></div>'
     } else {
         console.log(i);
-    }
-    
+        myContainer.innerHTML += '<div class="square"><p>'+ i +'</p></div>'
+    };
+
 }
